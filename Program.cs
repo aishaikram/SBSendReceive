62c804ed0e08d487d5a9ea862e1e4b5de5499772
Aisha Ikram <aisha.ikram@gmail.com>
2022-10-07 16:14:30 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Azure.Messaging.ServiceBus;
+
+Console.WriteLine("Hello, World!");
+string connectionString = "Endpoint=sb://<servicebusname>.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=mHHl0BS0LGIu10OGYLGWFgfHDAIEY4B/GEAi8B824Xc=";
+string queueName = "ai-queue";
+// since ServiceBusClient implements IAsyncDisposable we create it with "await using"
+await using var client = new ServiceBusClient(connectionString);
+
+// create the sender
+ServiceBusSender sender = client.CreateSender(queueName);
+
+// create a message that we can send. UTF-8 encoding is used when providing a string.
+ServiceBusMessage message = new ServiceBusMessage("Hello world from Console app using new package!");
+
+// send the message
+await sender.SendMessageAsync(message);
